9a8019c1e26cc469514b535ce7de68b979b93a0b
Dibya Ghosh <djgvolt@gmail.com>
2016-04-02 02:38:59 -0700

    Finished working Berkeley Guide Scraper

--- courses/scraper.py
+++ courses/scraper.py
@@ -1,32 +1,51 @@
 from lxml import html
 import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import *
 
-"""
-page = requests.get('http://guide.berkeley.edu/courses/')
-tree = html.fromstring(page.content)
-courses = tree.xpath('//*[@id="atozindex"]/ul/li/a')
-[print(i.text,i.attrib['href']) for i in courses]
-"""
-eecs = requests.get('http://guide.berkeley.edu/courses/eecs/')
-tree = html.fromstring(eecs.content)
-classes = tree.xpath('//div[@class="courseblock"]')
-[print(i.text) for i in classes]
+base_url = "http://guide.berkeley.edu"
+class GuideScraper:
+    def __init__(self):
+        self.engine = create_engine('sqlite:///courses.db', echo=False)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
 
-def elementToClass(e):
-    name  = e.xpath(".//span[@class='title']")[0].text
-    code  = e.xpath(".//span[@class='code']")[0].text
-    units = e.xpath(".//span[@class='hours']")[0].text
+    def get_courses(self):
+        page = requests.get(base_url+'/courses/')
+        tree = html.fromstring(page.content)
+        courses = tree.xpath('//*[@id="atozindex"]/ul/li/a')
+        [self.parse_department(i.attrib['href']) for i in courses]
 
-    coursedetails = e.xpath(".//p[@class='courseblockdesc']")[0]
-    desc = '\n'.join(v.itertext())
+    def parse_department(self,url):
+        print(url)
+        department = requests.get(base_url+url)
+        tree = html.fromstring(department.content)
+        classes = tree.xpath('//div[@class="courseblock"]')
+        [self.session.add(self.elementToClass(e)) for e in classes if self.elementToClass(e)]
+        self.session.commit()
 
-    tpw = c.xpath('.//*[text()[contains(.,"Fall and/or spring:")]]')[0].tail
-    prereqs = c.xpath('.//*[text()[contains(.,"Prerequisites")]]')[0].tail
+    def elementToClass(self,e):
+        try:
+            name  = e.xpath(".//span[@class='title']")[0].text
+            code  = e.xpath(".//span[@class='code']")[0].text
+            units = e.xpath(".//span[@class='hours']")[0].text
 
-    sbcl = c.xpath('.//*[text()[contains(.,"Course Level")]]')[0].tail
-    sub, clevel = sbcl.split('/')
+            coursedetails = e.xpath(".//p[@class='courseblockdesc']")[0]
+            desc = '\n'.join(coursedetails.itertext())
 
-    gf = c.xpath('.//*[text()[contains(.,"Grading")]]')[0].tail
-    grading, finals = gf.split('.')
+            tpw = e.xpath('.//*[text()[contains(.,"Fall and/or spring:")]]')[0].tail
+            prereqs = e.xpath('.//*[text()[contains(.,"Prerequisites")]]')[0].tail
 
-    return Course(name, code, units, desc, tpw, prereqs, sub, clevel, grading, finals)
+            sbcl = e.xpath('.//*[text()[contains(.,"Course Level")]]')[0].tail
+            sub, clevel = sbcl.split('/')
+
+            gf = e.xpath('.//*[text()[contains(.,"Grading")]]')[0].tail
+            grading, finals = gf.split('.')[:2]
+            print(name)
+            return Course(name, code, units, desc, tpw, prereqs, sub, clevel, grading, finals)
+        except:
+            return
+
+g = GuideScraper()
+g.get_courses()
